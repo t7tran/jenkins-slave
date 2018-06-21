eac5ba1fe3e9d16a34d1732a44de007033c22d03
Tien Tran <tiendung.tran@gmail.com>
2018-06-21 23:22:29 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+cp /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
 if [[ -f "$INIT_SCRIPT" ]]; then
 	cp $INIT_SCRIPT /tmp/init.sh
 	chmod +x /tmp/init.sh
