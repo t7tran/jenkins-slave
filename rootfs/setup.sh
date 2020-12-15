d7daacfad07391d3ab0ebddce38bc436589c2033
Tien Tran <tiendung.tran@gmail.com>
2020-12-15 22:13:50 +1100

    Install yarn dependency management tool for nodejs

--- rootfs/setup.sh
+++ rootfs/setup.sh
@@ -158,6 +158,9 @@ apt install /tmp/libjasper1.deb /tmp/libjasper-dev.deb
 #-------------------------------------------------------------------------
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install -y nodejs
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt update && apt install -y yarn
 npm install -g nexus-npm
 chown -R jenkins:jenkins /home/jenkins/{.config,.npm}
 
