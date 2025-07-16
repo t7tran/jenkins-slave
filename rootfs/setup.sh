213a557ee87dff3902b3ae9a40c8a2c7e4ae8e28
Tien Tran <tien@transfamily.info>
2025-07-16 18:21:23 +1000

    Install rsync utility

--- rootfs/setup.sh
+++ rootfs/setup.sh
@@ -30,7 +30,8 @@ apt-get install -y \
                 xvfb \
                 mysql-client \
                 git git-lfs \
-                skopeo
+                skopeo \
+                rsync
 # required by MariaDB4j
 # see https://github.com/MariaDB4j/MariaDB4j#faq
 curl -fsSLo /tmp/libtinfo.deb https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2_amd64.deb && dpkg -i /tmp/libtinfo.deb && rm -f /tmp/libtinfo.deb
